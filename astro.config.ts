aae9afcabfc41f8c1da8b273cb3efa4369a1ed8e
José Richiez <66890242+Wachamuli@users.noreply.github.com>
2026-03-19 11:09:27 -0500

    output changed from 'server' to 'static' (#22)

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: "https://richiezrijo.com/",
   integrations: [sitemap()],
   vite: {
