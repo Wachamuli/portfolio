83912d4eaa7abae2de2f4fcf81f0ad69ba5526de
José Richiez <66890242+Wachamuli@users.noreply.github.com>
2026-03-24 16:36:58 -0500

    Fix - readability issues (#23)
    * New tag component
    
    * slight changes to button, specially in Hero section
    
    * dimmed text with higher contrast
    
    * Better transition handling between light and dark theme
    
    * Opacity and width consistency changes
    
    * rounded corners for ActionLink
    
    * bug fix: navigation items send to the home route properly
    
    * GithubStats redesign
    
    * ProjectCard redesign

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   site: "https://richiezrijo.com/",
   integrations: [sitemap()],
   vite: {
